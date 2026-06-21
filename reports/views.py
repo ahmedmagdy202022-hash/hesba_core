8c0bbc5b08de36c055d9472e82f543a809b0a17f
ahmedmagdy202022-hash <ahmedmagdy202022@gmail.com>
2026-06-22 01:53:31 +0300

    090 simplify home view

--- reports/views.py
+++ reports/views.py
@@ -1,26 +1,5 @@
-from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render
-
-from reports.services import get_local_controlled_cycle_snapshot
+from django.http import HttpResponse
 
 
 def home(request):
-    snapshot = None
-    snapshot_ready = False
-    error_message = ""
-
-    try:
-        snapshot = get_local_controlled_cycle_snapshot()
-        snapshot_ready = True
-    except ObjectDoesNotExist:
-        error_message = "البيانات التجريبية لم تكتمل بعد. شغل seed_dev_master_data ثم controlled_cycle_smoke_test."
-
-    return render(
-        request,
-        "reports/home.html",
-        {
-            "snapshot": snapshot,
-            "snapshot_ready": snapshot_ready,
-            "error_message": error_message,
-        },
-    )
+    return HttpResponse("Hesba Core dashboard is ready. Open /admin/ for details.")
