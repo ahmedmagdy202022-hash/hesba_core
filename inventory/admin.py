7d14963da1b5a57bb915a0d4a45a76b6c7b43357
ahmedmagdy202022-hash <ahmedmagdy202022@gmail.com>
2026-06-21 16:07:57 +0300

    056 Show sales invoice in stock movement admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -5,7 +5,31 @@ from .models import StockMovement
 
 @admin.register(StockMovement)
 class StockMovementAdmin(admin.ModelAdmin):
-    list_display = ("movement_date", "movement_type", "item", "location", "quantity", "unit_cost", "purchase_invoice")
-    search_fields = ("item__item_code", "item__item_name", "location__location_code", "location__name_ar", "purchase_invoice__invoice_number")
+    list_display = (
+        "movement_date",
+        "movement_type",
+        "item",
+        "location",
+        "quantity",
+        "unit_cost",
+        "purchase_invoice",
+        "sales_invoice",
+    )
+    search_fields = (
+        "item__item_code",
+        "item__item_name",
+        "location__location_code",
+        "location__name_ar",
+        "purchase_invoice__invoice_number",
+        "sales_invoice__invoice_number",
+    )
     list_filter = ("movement_type", "movement_date", "location")
-    autocomplete_fields = ("item", "location", "purchase_invoice", "purchase_line", "created_by")
+    autocomplete_fields = (
+        "item",
+        "location",
+        "purchase_invoice",
+        "purchase_line",
+        "sales_invoice",
+        "sales_line",
+        "created_by",
+    )
