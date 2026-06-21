cf440b23c7c4056a34fa7ce0995121281bda770b
ahmedmagdy202022-hash <ahmedmagdy202022@gmail.com>
2026-06-22 01:53:48 +0300

    090 Add first dashboard URL

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin as django_admin
 from django.urls import path
 
+from reports.views import home
+
 
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", django_admin.site.urls),
 ]
