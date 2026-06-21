b5f2548c28060a5e4ff14facd393521566f18d08
ahmedmagdy202022-hash <ahmedmagdy202022@gmail.com>
2026-06-21 22:06:18 +0300

    071 Add closing period basic migration

--- closing/migrations/0001_initial.py
+++ closing/migrations/0001_initial.py
@@ -1,7 +1,18 @@
-from django.db import migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
     initial = True
     dependencies = []
-    operations = []
+    operations = [
+        migrations.CreateModel(
+            name="Period",
+            fields=[
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("period_code", models.CharField(max_length=80, unique=True)),
+                ("name", models.CharField(max_length=255)),
+                ("start_date", models.DateField()),
+                ("end_date", models.DateField()),
+            ],
+        ),
+    ]
