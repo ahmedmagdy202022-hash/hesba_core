cdb2083095e1ae9fd771c969ec917096b85119df
ahmedmagdy202022-hash <ahmedmagdy202022@gmail.com>
2026-06-22 00:14:22 +0300

    Update settings migration indexes

--- settings_core/migrations/0004_usage_index_names.py
+++ settings_core/migrations/0004_usage_index_names.py
@@ -1,6 +1,24 @@
-from django.db import migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
     dependencies = [("settings_core", "0003_usage_status_options")]
-    operations = []
+
+    operations = [
+        migrations.RemoveIndex(
+            model_name="usagestatussnapshot",
+            name="settings_us_status_2c9952_idx",
+        ),
+        migrations.RemoveIndex(
+            model_name="usagestatussnapshot",
+            name="settings_us_created_36d15b_idx",
+        ),
+        migrations.AddIndex(
+            model_name="usagestatussnapshot",
+            index=models.Index(fields=["status_level"], name="settings_co_status__04f34e_idx"),
+        ),
+        migrations.AddIndex(
+            model_name="usagestatussnapshot",
+            index=models.Index(fields=["created_at"], name="settings_co_created_0ddac57_idx"),
+        ),
+    ]
