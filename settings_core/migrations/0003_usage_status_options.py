eba8e34ea1958a976418e50cdfc2bbf57bae0ca3
ahmedmagdy202022-hash <ahmedmagdy202022@gmail.com>
2026-06-21 22:16:07 +0300

    072 Align usage status migration state

--- settings_core/migrations/0003_usage_status_options.py
+++ settings_core/migrations/0003_usage_status_options.py
@@ -1,6 +1,25 @@
-from django.db import migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
     dependencies = [("settings_core", "0002_usage_status_snapshot")]
-    operations = []
+
+    operations = [
+        migrations.AlterModelOptions(
+            name="usagestatussnapshot",
+            options={
+                "ordering": ["-created_at", "-id"],
+                "verbose_name": "Usage Status Snapshot",
+                "verbose_name_plural": "Usage Status Snapshots",
+            },
+        ),
+        migrations.AlterField(
+            model_name="usagestatussnapshot",
+            name="status_level",
+            field=models.CharField(
+                choices=[("green", "Green"), ("yellow", "Yellow"), ("orange", "Orange"), ("red", "Red")],
+                default="green",
+                max_length=20,
+            ),
+        ),
+    ]
