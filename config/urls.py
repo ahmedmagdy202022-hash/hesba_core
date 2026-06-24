5800a0ac5876c062793708f4f5e3806d9d29d94d
ahmedmagdy202022-hash <ahmedmagdy202022@gmail.com>
2026-06-24 08:55:57 +0300

    108 add login routes

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin as django_admin
 from django.urls import path
 
+from accounts.views import HesbaLoginView, HesbaLogoutView
 from reports.status_views import status_counts_report
 from reports.views import dashboard_snapshot, home, report_hub
 
 
 urlpatterns = [
+    path("login/", HesbaLoginView.as_view(), name="login"),
+    path("logout/", HesbaLogoutView.as_view(), name="logout"),
     path("", home, name="home"),
     path("dashboard/", dashboard_snapshot, name="dashboard_snapshot"),
     path("reports/", report_hub, name="report_hub"),
