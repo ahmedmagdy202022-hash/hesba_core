49906851f55711214ca4d1a7d28edc47314676e3
ahmedmagdy202022-hash <ahmedmagdy202022@gmail.com>
2026-06-24 23:19:35 +0300

    Route login shell before dashboard

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin as django_admin
 from django.urls import path
+from django.views.generic import RedirectView
 
+from accounts.views import login_shell
 from reports.status_views import status_counts_report
 from reports.views import dashboard_snapshot, home, report_hub
 
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", RedirectView.as_view(pattern_name="login", permanent=False), name="root_redirect"),
+    path("login/", login_shell, name="login"),
+    path("home/", home, name="home"),
     path("dashboard/", dashboard_snapshot, name="dashboard_snapshot"),
     path("reports/", report_hub, name="report_hub"),
     path("status/", status_counts_report, name="status_counts_report"),
