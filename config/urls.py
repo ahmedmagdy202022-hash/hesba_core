f4c9972576a93c747cd30969fff151811bacb9fb
ahmedmagdy202022-hash <ahmedmagdy202022@gmail.com>
2026-06-26 00:53:56 +0300

    115A add setup gate web route

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin as django_admin
 from django.urls import path
-from django.views.generic import RedirectView
-from django.contrib.auth.views import LoginView
+from django.views.generic import RedirectView, TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from reports.status_views import status_counts_report
 from reports.views import dashboard_snapshot, home, report_hub
@@ -10,6 +10,8 @@ from reports.views import dashboard_snapshot, home, report_hub
 urlpatterns = [
     path("", RedirectView.as_view(pattern_name="login", permanent=False), name="root_redirect"),
     path("login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
+    path("setup/", TemplateView.as_view(template_name="setup/setup_gate.html"), name="setup_gate"),
     path("home/", home, name="home"),
     path("dashboard/", dashboard_snapshot, name="dashboard_snapshot"),
     path("reports/", report_hub, name="report_hub"),
